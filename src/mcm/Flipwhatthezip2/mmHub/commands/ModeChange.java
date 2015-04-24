5c85cb5b85aedd36cabe523d2aed055e666674f7
flipwhatthezip2 <flipwhatthezip2.mc@gmail.com>
2015-04-24 14:08:47 -0400

    Added server modes: 'Updating' + 'Locked' / Cleaned up messy code

--- src/mcm/Flipwhatthezip2/mmHub/commands/ModeChange.java
+++ src/mcm/Flipwhatthezip2/mmHub/commands/ModeChange.java
@@ -22,8 +22,10 @@ import org.bukkit.command.CommandSender;
 public class ModeChange implements CommandExecutor{
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String string, String[] args) {
-        if (cmd.getName().equalsIgnoreCase("betamode")){
-            if (Core.beta == true){
+        if (cmd.getName().equalsIgnoreCase("servermode")){
+         if (args.length == 1){
+          if (args[0].equalsIgnoreCase("beta")){
+           if (Core.beta == true){
                 sender.sendMessage("");
                 sender.sendMessage("");
                 sender.sendMessage("");
@@ -33,7 +35,7 @@ public class ModeChange implements CommandExecutor{
                 sender.sendMessage("");
                 sender.sendMessage("");
                 Core.beta = false;
-            } else {
+            } else if(Core.beta == false){
                 sender.sendMessage("");
                 sender.sendMessage("");
                 sender.sendMessage("");
@@ -43,7 +45,51 @@ public class ModeChange implements CommandExecutor{
                 sender.sendMessage("");
                 sender.sendMessage("");
                 Core.beta = true;
+          } else if(args[0].equalsIgnoreCase("updating")){
+           if(Core.updating == true){
+                sender.sendMessage("");
+                sender.sendMessage("");
+                sender.sendMessage("");
+                sender.sendMessage(Core.warning + "Updating mode un-toggled.");
+                sender.sendMessage(Core.warning + "Players will now be able to join the server.");
+                sender.sendMessage("");
+                sender.sendMessage("");
+                sender.sendMessage("");
+                Core.updating = false;
+            } else if(Core.updating == false){
+                sender.sendMessage("");
+                sender.sendMessage("");
+                sender.sendMessage("");
+                sender.sendMessage(Core.warning + "Updating mode toggled.");
+                sender.sendMessage(Core.warning + "Players will §lnot §cbe able to join the server.");
+                sender.sendMessage("");
+                sender.sendMessage("");
+                sender.sendMessage("");
+                Core.updating = true;
+            } else if(args[0].equalsIgnoreCase("lock")) {
+             if(Core.locked == true){
+                sender.sendMessage("");
+                sender.sendMessage("");
+                sender.sendMessage("");
+                sender.sendMessage(Core.warning + "Locked mode un-toggled.");
+                sender.sendMessage(Core.warning + "Players will now be able to join the server.");
+                sender.sendMessage("");
+                sender.sendMessage("");
+                sender.sendMessage("");
+                Core.locked = false;
+            } else if(Core.locked == false){
+                sender.sendMessage("");
+                sender.sendMessage("");
+                sender.sendMessage("");
+                sender.sendMessage(Core.warning + "Locked mode toggled.");
+                sender.sendMessage(Core.warning + "Players will §lnot §cbe able to join the server.");
+                sender.sendMessage("");
+                sender.sendMessage("");
+                sender.sendMessage("");
+                Core.locked = true;
             }
+         } else {
+            sender.sendMessage(Core.warning + "§lUSAGE: §c/servermode [beta, updating, locked]");
         }
         return false;
     }

9bde69d0de946cee995f048976b78243f46ed321
flipwhatthezip2 <flipwhatthezip2.mc@gmail.com>
2015-04-24 08:53:43 -0400

    Debugged 'Beta Mode' disconnect protocol.

--- src/mcm/Flipwhatthezip2/mmHub/events/PlayerPRELogin.java
+++ src/mcm/Flipwhatthezip2/mmHub/events/PlayerPRELogin.java
@@ -20,17 +20,18 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerPreLoginEvent;
 
-public class PlayerPRELogin implements Listener{
+public class PlayerPRELogin implements Listener {
 
     @EventHandler
-    public void PlayerPRELogin(AsyncPlayerPreLoginEvent e){
+    public void PlayerPRELogin(AsyncPlayerPreLoginEvent e) {
         String p = e.getName();
         if (Core.beta == true) {
-            if (p.equalsIgnoreCase("Flipwhatthezip2") || p.equalsIgnoreCase("Maxwell3103")){
+            if (p.equalsIgnoreCase("Flipwhatthezip2") || p.equalsIgnoreCase("Maxwell3103")) {
                 return;
+            } else {
+                e.disallow(AsyncPlayerPreLoginEvent.Result.KICK_OTHER, "§4§l>> §c§lUH OH! §4§l<< \n" +
+                        "§cPlayer joining is not permitted whilst the server is in §lBETA MODE!");
             }
-            e.disallow(AsyncPlayerPreLoginEvent.Result.KICK_OTHER, "§4§l>> §c§lUH OH! §4§l<< \n" +
-                    "§cPlayer joining is not permitted whilst the server is in §lBETA MODE!");
         }
     }
 }
